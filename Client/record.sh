a23389fd7d6f89c770f864e3032db0237f7f1cb6
bak siu <atomicw63.546@gmail.com>
2025-12-14 13:39:16 +0900

    Adjust framerate in recording script from 30 to 24 fps for improved video quality

--- Client/record.sh
+++ Client/record.sh
@@ -7,7 +7,7 @@ rpicam-vid -t 0 --inline -n \
     --segment 120000 \
     --bitrate 10000000 \
     --profile high \
-    --framerate 30 \
+    --framerate 24 \
     --width 1640 \
     --height 1232 \
     -o /home/pi/cctv_buffer/${HOSTNAME}_${DATE}_%05d.h264
